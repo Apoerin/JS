d908349fe3d5e000e4061e479e996e5226455b15
Nastya <apoerin73@gmail.com>
2019-01-27 13:01:21 +0200

    resolved mistakes

--- randomize.js
+++ randomize.js
@@ -1,88 +1,87 @@
  'use strict';
-        document.addEventListener('DOMContentLoaded', function () {
-            function getRandomInt(min, max) {
-                return Math.floor(Math.random() * (max - min)) + min;
+document.addEventListener('DOMContentLoaded', function () {
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    let limitNumber;
+    let pageNumber;
+
+    setTimeout(function () {
+       limitNumber = getRandomInt(1, 11);
+        console.log(limitNumber)
+    }, 1000);
+    setTimeout(function () {
+        pageNumber = getRandomInt(1, 11);
+        console.log(pageNumber)
+    }, 2000);
+
+    setTimeout(function () {
+        makeRequest(function (responseText) {
+            console.log("callback response =>", JSON.parse(responseText));
+        })
+    }, 3000);
+
+
+
+    setTimeout(getPromise, 3000);
+    setTimeout(makeAsyncRequest, 3000);
+
+    // Callback
+
+    function makeRequest(callback) {
+        let xmlRequest = new XMLHttpRequest();
+        xmlRequest.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                callback(this.responseText);
             }
-
-            let limitNumber;
-            let pageNumber;
-
-            setTimeout(function () {
-                limitNumber = getRandomInt(1, 11);
-                console.log(limitNumber)
-            }, 1000);
-            setTimeout(function () {
-                pageNumber = getRandomInt(1, 11);
-                console.log(pageNumber)
-            }, 2000);
-
-            setTimeout(function () {
-                getCallback(function (responseText) {
-                    console.log("callback response =>", JSON.parse(responseText));
-                })
-            }, 3000);
-
-            setTimeout(getPromise, 3000);
+        };
+            xmlRequest.open("GET", `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
+            xmlRequest.send();
+        }
 
 
-            // Callback
+    // Async Await
 
-
-            function getCallback(callback) {
-                let xmlRequest = new XMLHttpRequest();
-                xmlRequest.onreadystatechange = function () {
-                    if (this.readyState === 4 && this.status === 200) {
-                        callback(this.responseText);
-                    }
-                };
-                xmlRequest.open("GET", `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
-                xmlRequest.send();
-            }
-            getCallback();
-
-
-            // Async Await
-
-
-            async function getAsync() {
-                let xmlRequest = new XMLHttpRequest();
-                xmlRequest.onreadystatechange = function () {
-                    if (this.readyState === 4 && this.status === 200) {
-                        return this.responseText;
-                    }
-                };
-                xmlRequest.open('GET', 'http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}');
-                xmlRequest.send();
-            }
-
-            getAsync().then((response) => {
-                console.log(JSON.parse(response));
-            });
-
-             // Promise
-
-
-            function getPromise() {
-                let promiseRequest = new Promise((resolve, reject) => {
-                    let xmlRequest = new XMLHttpRequest();
-                    xmlRequest.onreadystatechange = function () {
-                        if (this.readyState === 4 && this.status === 200) {
-                            resolve(this.responseText);
-                            reject(this.status);
-                        }
-                    };
-                    xmlRequest.open("GET", `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
-                    xmlRequest.send();
-                });
-
-                promiseRequest.then((responseText) => {
-                    console.log("promise response =>", JSON.parse(responseText));
-                }).catch((status) => {
-                    console.log("promise status =>", JSON.parse(status));
-                });
-            }
-            getPromise();
+    async function getAsync() {
+        return new Promise((resolve)=> {
+            let xmlRequest = new XMLHttpRequest();
+            xmlRequest.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    resolve(this.responseText);
+                }
+            };
+            xmlRequest.open("GET", `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
+            xmlRequest.send();
         });
+    }
+    async function makeAsyncRequest() {
+        try{
+            let result = await getAsync();
+            console.log("async response  =>",JSON.parse(result));
+        }catch (error) {
+            console.error(error);
+        }
+    }
 
 
 
+    // Promise
+
+    function getPromise() {
+        let promiseRequest = new Promise((resolve, reject) => {
+            let xmlRequest = new XMLHttpRequest();
+            xmlRequest.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    resolve(this.responseText);
+                }
+            };
+            xmlRequest.open("GET", `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
+            xmlRequest.send();
+        });
+        promiseRequest.then((responseText) => {
+            console.log("promise response =>", JSON.parse(responseText));
+        }).catch((status) => {
+            console.log("promise status =>", JSON.parse(status));
+        });
+    }
+});
